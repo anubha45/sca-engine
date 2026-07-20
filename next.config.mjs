73322030f815cb15aaf5371f574950b832f46abd
anubha45 <anubhavgupta0910@gmail.com>
2026-07-20 12:43:12 +0530

    revert to standard next config for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output:"export",
-  images:{unoptimized:true},
-  basePath: '/sca-engine' || "",
-  assetPrefix: '/sca-engine' || "",
+ 
 };
 
 export default nextConfig;
